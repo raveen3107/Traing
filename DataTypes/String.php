671c80537ab8d0697fa5dc8035e4282eb56b1220
Venkataraveen Gangireddy <raveen3107@gmail.com>
2018-04-24 11:13:00 +0530

    first commit

---
+++ DataTypes/String.php
@@ -0,0 +1,9 @@
+<?php
+$x="Hello my name is raveen";
+$y="may i know your name please";
+
+echo $x;
+echo "<br>";
+echo $y;
+
+?>
---
+++ variables/localvariables.php
@@ -0,0 +1,11 @@
+<?php
+ // global scope
+ 
+function myTest() {
+    $x = 5;
+    echo "<p>Variable x inside function is: $x</p>";
+} 
+myTest();
+
+echo "<p>Variable x outside function is: $x</p>";
+?>
