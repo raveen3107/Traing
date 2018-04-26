eee4b2eda37717c06583625837f6ea2ff3df1396
raveen3107 <raveen3107@gmail.com>
2018-04-26 17:27:17 +0530

    third commit

---
+++ PhpProject1/data/search.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ * 
+ */
+$get = $_POST['key'];
+//echo $temp;
+//$input = preg_quote($temp, '~'); // don't forget to quote input string!
+$data = array('orange', 'blue', 'green', 'red', 'pink', 'brown', 'black');
+
+$result = preg_grep('~' . $get. '~', $data);
+print_r($result);
+?>
